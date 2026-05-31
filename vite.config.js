21a043b06275e602450baa2559d6f37ae79dacf5
Zoe Haenen <12502617@student.pxl.be>
2026-05-31 22:32:57 +0200

    Use root base outside GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const base = process.env.GITHUB_ACTIONS ? '/portfolio-wpl2/' : '/'
+
 export default defineConfig({
-  base: '/portfolio-wpl2/',
+  base,
   plugins: [vue()],
 })
